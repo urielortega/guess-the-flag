2fc4f1e213bc9f98a5cff605538811f57bd297fc
Uriel Ortega <urielortega2509@gmail.com>
2023-06-26 16:46:10 -0600

    Implemented accessibility features.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    var country: String
+    
+    var body: some View {
+        Image(country)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .shadow(radius: 5)
+    }
+}
+
 struct BlueTitle: ViewModifier {
     func body(content: Content) -> some View {
         content
@@ -40,16 +51,19 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
-    struct FlagImage: View {
-        var country: String
-        
-        var body: some View {
-            Image(country)
-                .renderingMode(.original)
-                .clipShape(Capsule())
-                .shadow(radius: 5)
-        }
-    }
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]    
     
     var body: some View {
         ZStack {
@@ -94,6 +108,7 @@ struct ContentView: View {
                                 .scaleEffect(flagWasTapped && number != tappedFlag ? 0.5 : 1)
                                 .animation(.default, value: flagWasTapped)
                                 .rotation3DEffect(.degrees(rotationDegrees), axis: (x: 0, y: 1, z: 0))
+                                .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         }
                     }
                 }
